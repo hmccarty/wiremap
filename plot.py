09625cc923fce766061c0e600199b9fd170e7b84
Harrison McCarty <hmccarty@pm.me>
2023-03-02 22:43:00 -0500

    added live draw

--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
+import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import os
 import sys
+import numpy as np
 import ipaddress
 
 def get_hilbert_point(n, d):
@@ -31,16 +33,36 @@ def get_hilbert_point(n, d):
         s *= 2
     return x, y
 
-def main():
+def on_close(event):
+    print("Exiting...")
+    exit()
+
+def main(): 
     num_ips = 4294967296
+    start_address = "192.168.0.1"
+    end_address = "255.255.255.255"
+
+    def d2ip(d):
+        return [str(ipaddress.ip_address(int(i))) for i in d]
+    
+    # Setup plot
+    fig, ax = plt.subplots()
+    fig.canvas.mpl_connect('close_event', on_close)
 
     # Create hilbert curve of all IPs 
     all_ip_x = []
     all_ip_y = []
-    for d in range(num_ips):
+    for d in range(int(ipaddress.ip_address(start_address)), int(ipaddress.ip_address(end_address))):
         x, y = get_hilbert_point(num_ips, d)
         all_ip_x.append(x)
         all_ip_y.append(y)
+
+        # TODO: Fix incorrect labels
+        ax.set_yticklabels(d2ip(all_ip_y), rotation=45)
+        ax.set_xticklabels(d2ip(all_ip_x), rotation=45)
+        ax.plot(all_ip_x, all_ip_y)
+        plt.draw()
+        plt.pause(0.001)
 
     # Source connected IPs
     if len(sys.argv) < 2:
@@ -61,9 +83,8 @@ def main():
             my_ip_y.append(y)
 
     # Plot data
-    fig, ax = plt.subplots()
-    ax.plot(all_ip_x, all_ip_y)
-    ax.plot(my_ip_x, my_ip_y, 'o')
+    # ax.plot(all_ip_x, all_ip_y)
+    # ax.plot(my_ip_x, my_ip_y, 'o')
 
     plt.show()
 
